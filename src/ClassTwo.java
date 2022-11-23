0c2e0c5ad1d22fb4b38ca3183a6d3b2fa464c8cb
jensenedutest1 <jensenedutest1@gmail.com>
2022-11-23 13:55:50 +0100

    a new change that y'all don't have

--- src/ClassTwo.java
+++ src/ClassTwo.java
@@ -3,5 +3,6 @@ public class ClassTwo
     void myfunc()
     {
         System.out.println("this is from branch feature1");
+        System.out.println("a second change y'all don't have");
     }
 }
